e80d29a1c71b8531a74d51d5ac88d899438067ef
Pavel Khomich <pashax0@mail.ru>
2019-09-18 23:08:13 +0300

    feat: add and first styling main block

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-let showMenuBtn = document.querySelector('.burger-btn');
 let mainMenu = document.querySelector('.main-menu')
 document.onclick = (e) => {
     if (e.target.className == "burger-btn") {
-        mainMenu.classList.toggle("visible")
+        mainMenu.classList.toggle("visible");
+    } else {
+        mainMenu.classList.remove("visible");
     }
 };
